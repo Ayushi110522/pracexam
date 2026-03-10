f54a942e2c783ec1f372b4ba4124d272f61cac82
Ayushi110522 <157972932+Ayushi110522@users.noreply.github.com>
2026-03-10 13:10:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome Aayushi</h1>
+      <Home/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
